1856c660ab0c2c3185087cc010253841e99ef1bb
black1987 <ahdidou.mohamed@gmail.com>
2013-03-09 18:42:57 +0000

    switch to dev from terminal

--- iFootBall/src/com/ahdidou/ifootball/MainActivity.java
+++ iFootBall/src/com/ahdidou/ifootball/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		Log.d(TAG,"Switched to DEv from terminal");
 		
 	}
 
